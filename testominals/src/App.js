96944aa59a38345ae6b3e2468004a233b251cc43
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-07-16 18:57:05 +0530

    changes in testimonials slider webapp and download intial setup for formapp

--- testominals/src/App.js
+++ testominals/src/App.js
@@ -4,7 +4,7 @@ import reviews from "./data";
 
 const App = () => {
   return (
-    <div className="flex flex-col w-[100vw] h-[100%] justify-center items-center bg-gray-200">
+    <div className="flex flex-col w-[100vw] h-[100%] md:h-[100vh] justify-center items-center bg-gray-200">
       <div className="text-center mb-8 md:mb-0">
         <h1 className="text-4xl font-bold my-8 md:my-0">Our Testimonial</h1>
         <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto rounded-lg"></div>
